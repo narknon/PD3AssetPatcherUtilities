8ce45c28da3e0796f54eea24d59ad5c1ebd44d6d
narknon <73571427+narknon@users.noreply.github.com>
2023-10-11 21:39:21 -0400

    Updates to extract wwise data and other stuff

--- PD3AssetPatcher/Program.cs
+++ PD3AssetPatcher/Program.cs
@@ -1,11 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Newtonsoft.Json.Linq;
 using UAssetAPI;
 using UAssetAPI.ExportTypes;
 using UAssetAPI.PropertyTypes.Objects;
 using UAssetAPI.UnrealTypes;
+using System.IO;
 
-
+static bool ByteArrayToFile(string fileName, byte[] byteArray)
+{
+    try
+    {
+        using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+        {
+            fs.Write(byteArray, 0, byteArray.Length);
+            return true;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Exception caught in process: {0}", ex);
+        return false;
+    }
+}
 
 
 Console.Write("Init\n");
@@ -17,6 +34,8 @@ if (args.Length == 0)
     Console.WriteLine("\nUsage:");
     Console.WriteLine("PD3AssetPatcher.exe AnimPatch \"path/to/assets/\"");
     Console.WriteLine("PD3AssetPatcher.exe AudioPatch \"path/to/assets/\"");
+    Console.WriteLine("PD3AssetPatcher.exe AudioInfo \"path/to/assets/\"");
+    Console.WriteLine("PD3AssetPatcher.exe ExtractWWiseData \"path/to/assets/\"");
     return;
 }
 
@@ -35,6 +54,14 @@ if (Directory.Exists(args[1]))
         {
             PatchAudioData(assetPath);
         }
+        else if (args[0] == "AudioInfo")
+        {
+            GetAudioDataInfo(assetPath);
+        }
+        else if (args[0] == "ExtractWWiseData")
+        {
+            ExtractWWiseData(assetPath);
+        }
         else
         {
             Console.WriteLine("Invalid Argument Entered");
@@ -50,6 +77,14 @@ else if (File.Exists(args[1]))
     else if (args[0] == "AudioPatch")
     {
         PatchAudioData(args[1]);
+    }
+    else if (args[0] == "AudioInfo")
+    {
+        GetAudioDataInfo(args[1]);
+    }
+    else if (args[0] == "ExtractWWiseData")
+    {
+        ExtractWWiseData(args[1]);
     }
     else
     {
@@ -152,34 +187,34 @@ static void PatchAudioData(string assetPath)
 
                 int locationOfHeader = Search(export.Extras, BitConverter.GetBytes(value));
 
-                int locationOf2ndMatchHeader = Search(export.Extras, BitConverter.GetBytes(value), locationOfHeader + 4);
+                int locationOf2ndMatchHeader = Search(export.Extras, BitConverter.GetBytes(value), locationOfHeader + 4) + 4;
 
                 if (locationOf2ndMatchHeader != -1)
                 {
-                    double durationDouble = GetDoubleInput("Enter Audio Duration in milliseconds:");
+                    double fPlayAt = GetDoubleInput("Enter Play At in milliseconds:");
 
-                    float fadeInStartFloat = GetFloatInput("Enter Fade-In Start in seconds:");
+                    double fBeginTrimOffset = GetDoubleInput("Enter Begin Trim-Offset in seconds:");
 
-                    float fadeInEndFloat = GetFloatInput("Enter Fade-In End in seconds:");
+                    double fEndTrimOffset = GetDoubleInput("Enter End Trim-Offset in seconds:");
 
-                    float fadeOutStartFloat = GetFloatInput("Enter Fade-Out Start in seconds:");
+                    double fSrcDuration = GetDoubleInput("Enter Source Duration in milliseconds:");
 
-                    float fadeOutEndFloat = GetFloatInput("Enter Fade-Out End in seconds:");
+                    /*float fadeOutEndFloat = GetFloatInput("Enter Fade-Out End in seconds:");
 
                     double maxDurationDouble = GetDoubleInput("Enter Maximum Music Duration in milliseconds:");
 
                     double loopStartDouble = GetDoubleInput("Enter Loop-Start in milliseconds:");
 
-                    double loopEndDouble = GetDoubleInput("Enter Loop-End in milliseconds:");
+                    double loopEndDouble = GetDoubleInput("Enter Loop-End in milliseconds:");*/
 
-                    Buffer.BlockCopy(BitConverter.GetBytes(durationDouble), 0, export.Extras, locationOf2ndMatchHeader + 0x54, 8);
-                    Buffer.BlockCopy(BitConverter.GetBytes(fadeInStartFloat), 0, export.Extras, locationOf2ndMatchHeader + 0xA4 - 0x34, 4);
-                    Buffer.BlockCopy(BitConverter.GetBytes(fadeInEndFloat), 0, export.Extras, locationOf2ndMatchHeader + 0xB0 - 0x34, 4);
-                    Buffer.BlockCopy(BitConverter.GetBytes(fadeOutStartFloat), 0, export.Extras, locationOf2ndMatchHeader + 0xD4 - 0x34, 4);
-                    Buffer.BlockCopy(BitConverter.GetBytes(fadeOutEndFloat), 0, export.Extras, locationOf2ndMatchHeader + 0xE0 - 0x34, 4);
-                    Buffer.BlockCopy(BitConverter.GetBytes(maxDurationDouble), 0, export.Extras, locationOf2ndMatchHeader + 0x163 - 0x34, 8);
-                    Buffer.BlockCopy(BitConverter.GetBytes(loopStartDouble), 0, export.Extras, locationOf2ndMatchHeader + 0x173 - 0x34, 8);
-                    Buffer.BlockCopy(BitConverter.GetBytes(loopEndDouble), 0, export.Extras, locationOf2ndMatchHeader + 0x180 - 0x34, 8);
+                    Buffer.BlockCopy(BitConverter.GetBytes(fPlayAt), 0, export.Extras, locationOf2ndMatchHeader + 0x54, 8);
+                    Buffer.BlockCopy(BitConverter.GetBytes(fBeginTrimOffset), 0, export.Extras, locationOf2ndMatchHeader + 0x70, 8);
+                    Buffer.BlockCopy(BitConverter.GetBytes(fEndTrimOffset), 0, export.Extras, locationOf2ndMatchHeader + 0x7C, 8);
+                    Buffer.BlockCopy(BitConverter.GetBytes(fSrcDuration), 0, export.Extras, locationOf2ndMatchHeader + 0xA0, 8);
+                    /*Buffer.BlockCopy(BitConverter.GetBytes(fadeOutEndFloat), 0, export.Extras, locationOf2ndMatchHeader + 0xAC, 4);
+                    Buffer.BlockCopy(BitConverter.GetBytes(maxDurationDouble), 0, export.Extras, locationOf2ndMatchHeader + 0x12F, 8);
+                    Buffer.BlockCopy(BitConverter.GetBytes(loopStartDouble), 0, export.Extras, locationOf2ndMatchHeader + 0x13F, 8);
+                    Buffer.BlockCopy(BitConverter.GetBytes(loopEndDouble), 0, export.Extras, locationOf2ndMatchHeader + 0x14C, 8);*/
 
                     myAsset.Write(assetPath);
                     Console.Write("Asset Patched\n");
@@ -188,8 +223,6 @@ static void PatchAudioData(string assetPath)
                 {
                     Console.Write("Match not found\n");
                 }
-
-                
             }
         }
 
@@ -220,3 +253,36 @@ static void PatchAudioData(string assetPath)
     }
 }
 
+static void GetAudioDataInfo(string assetPath)
+{
+
+}
+
+static void ExtractWWiseData(string assetPath)
+{
+    Console.Write("Found asset: ");
+    Console.Write(assetPath);
+    Console.Write("\n");
+    UAsset myAsset = new UAsset(assetPath, EngineVersion.VER_UE4_27);
+    var bnkPattern = new byte[] { 0x42, 0x4B, 0x48, 0x44 };
+    var audioPattern = new byte[] { 0x52, 0x49, 0x46, 0x46 };
+    foreach (Export export in myAsset.Exports)
+    {
+        if (export.GetExportClassType().Value.Value == "AkAudioEventData" || export.GetExportClassType().Value.Value == "AkInitBankAssetData")
+        {
+            Console.Write("Found Ak Event\n");
+            int locationOfHeader = Search(export.Extras, bnkPattern);
+            byte[] bnkData = new byte[export.Extras.Length - locationOfHeader];
+            Array.ConstrainedCopy(export.Extras, locationOfHeader, bnkData, 0x0, export.Extras.Length - locationOfHeader);
+            string newAssPathConcat = String.Concat((assetPath.Split("."))[0], ".bnk");
+            ByteArrayToFile(newAssPathConcat, bnkData);
+        }
+        else if (export.GetExportClassType().Value.Value == "AkMediaAssetData")
+        {
+            Console.Write("Found Ak Media\n");
+            int locationOfHeader = Search(export.Extras, audioPattern);
+            string ubulkPath = String.Concat((assetPath.Split("."))[0], ".ubulk");
+            File.Copy(ubulkPath, String.Concat((assetPath.Split("."))[0], ".wav"));
+        }
+    }
+}
